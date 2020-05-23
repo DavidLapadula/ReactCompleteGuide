509c3fcbffb5c672b4dd6838377cc467a20225a1
DavidLapadula <lapadula.david@gmail.com>
2020-05-23 12:17:09 -0400

    [http-req] axios config complete

---
+++ HTTPReq/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axios from "axios";
+
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com"; 
+
+axios.interceptors.request.use(request => {
+    return request; 
+}, error => {
+    return Promise.reject(error); 
+}); 
+
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
+registerServiceWorker();
