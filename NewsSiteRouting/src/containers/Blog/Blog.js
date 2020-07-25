0aa1d9444a8e04ef61a7ae7a750c44d41511c94f
DavidLapadula <lapadula.david@gmail.com>
2020-07-25 11:42:11 -0400

    [burger-builder-routing] Used lazy loading with React Suspense

--- NewsSiteRouting/src/containers/Blog/Blog.js
+++ NewsSiteRouting/src/containers/Blog/Blog.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink, Switch } from 'react-router-dom';
-import FullPost from './FullPost/FullPost'
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent'; 
+const AsyncNewPost = asyncComponent(() => {
+    return import("./NewPost/NewPost"); 
+});
 
 class Blog extends Component {
     render () {
@@ -30,8 +32,9 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/new-post" component={AsyncNewPost} />
                     <Route path="/posts" component={Posts} />
+                    <Redirect from="/" to="/posts" />
                 </Switch>
             </div>
         );
