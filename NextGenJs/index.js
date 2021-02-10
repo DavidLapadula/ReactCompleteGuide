3107f7e6ba514193804d31cb5b685780ead4ad6c
DavidLapadula <lapadula.david@gmail.com>
2021-02-09 22:12:35 -0500

    [next-gen-js] basic examples of es6 additions

---
+++ NextGenJs/index.js
@@ -0,0 +1,12 @@
+// Arrow fn syntax
+const myFunc = (string) => {
+    console.log(string)
+}; 
+
+// Exactly 1 argument syntax
+const myFunc2 = string => {
+    console.log(string)
+}; 
+
+// Just a one line return
+const myFunc3 = number => number * 2; 
