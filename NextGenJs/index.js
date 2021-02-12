4179a4695ae85c30a8a0da1b8b4202601bbd7b98
DavidLapadula <lapadula.david@gmail.com>
2021-02-11 22:33:40 -0500

    [next-gen-js] add import/export and class examples

--- NextGenJs/index.js
+++ NextGenJs/index.js
@@ -10,3 +10,30 @@ const myFunc2 = string => {
 
 // Just a one line return
 const myFunc3 = number => number * 2; 
+
+// Class
+class Human {
+    constructor() {
+        this.gender = "Male";
+    }
+        
+    printGender() {
+        console.log(this.gender);
+    }
+    
+}
+
+class Person extends Human {
+    constructor() {
+        super();
+        this.name = "David";
+    }
+
+    printName() {
+        console.log(this.name);
+    }
+}
+
+const david = new Person();
+david.printName();
+david.printGender();
