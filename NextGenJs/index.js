524eb3e2fd344fc41f8e40be777861eda6e867c2
DavidLapadula <lapadula.david@gmail.com>
2021-02-13 12:26:23 -0500

    [next-gen-js] finish module with spread, rest, destructuring

--- NextGenJs/index.js
+++ NextGenJs/index.js
@@ -34,6 +34,42 @@ class Person extends Human {
     }
 }
 
-const david = new Person();
-david.printName();
-david.printGender();
+// Modern syntax
+class Human2 {
+    gender = 'male';
+
+    printGender = () => {
+        console.log(this.gender);
+    }
+}
+
+// Spread
+const arr1 = [1, 2, 3];
+const arr2 = [...arr1, 4, 5];
+
+// Rest
+function sort(...args){
+    return args.filter(el => el === 1);
+}
+
+// Destructuring
+const numbers = [1, 2, 3];
+[num1, num2] = numbers; // order matters
+
+const obj3 = {lName: 'smith'};
+{lName} = obj3;
+
+// Reference types 
+const testPerson = {
+    name: 'john'
+};
+
+const testPerson2 = {
+    ...person
+};
+
+person.name = 'jane'; // testPerson2 untouched because spread makes deep copy
+
+// Array functions
+
+
