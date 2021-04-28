0fee2eb7f7e4b24903d5e85bdb96acdd20f159eb
DavidLapadula <lapadula.david@gmail.com>
2021-04-27 22:30:51 -0400

    [base-features] Added custom component using props

--- BaseFeatures/base-features/src/App.js
+++ BaseFeatures/base-features/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from "./Person/Person"; 
 
 class App extends Component {
   render() {
     return (
-      <h1>
-        This is react.
-      </h1>
+      <div>
+        <h1>
+          This is react.
+        </h1>
+        <Person name="David" age="27"/>
+        <Person name="David1" age="28">This is fun</Person>
+      </div>
     );
   }
 }
