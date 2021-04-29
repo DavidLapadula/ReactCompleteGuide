d344ff36d608d12453439479a4adfc2b1592266c
DavidLapadula <lapadula.david@gmail.com>
2021-04-28 22:39:12 -0400

    [base-features] practiced using setState and props in class component

--- BaseFeatures/base-features/src/App.js
+++ BaseFeatures/base-features/src/App.js
@@ -3,14 +3,31 @@ import './App.css';
 import Person from "./Person/Person"; 
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "David", age: 28},
+      {name: "Mike", age: 29},
+    ]
+  }
+
+  switchNameHandler = () => {
+      this.setState({
+       persons: [
+        {name: "David1", age: 28},
+        {name: "Mike1", age: 29},
+       ]
+      })
+  }
+
   render() {
     return (
       <div>
         <h1>
           This is react.
         </h1>
-        <Person name="David" age="27"/>
-        <Person name="David1" age="28">This is fun</Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
       </div>
     );
   }
