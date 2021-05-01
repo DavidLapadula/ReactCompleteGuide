215475b51ba508630d1ae4179f020c00003e2b29
DavidLapadula <lapadula.david@gmail.com>
2021-05-01 16:28:36 -0400

    [base-features] Added two way binding to list

--- BaseFeatures/base-features/src/App.js
+++ BaseFeatures/base-features/src/App.js
@@ -22,23 +22,40 @@ const app = props => {
 
   const [otherState, setOtherState] = useState("Some other state stuff")
 
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
       setPersonsState({
          persons: [
-          {name: "David1", age: 28},
+          {name: newName, age: 28},
           {name: "Mike1", age: 29},
          ]
       })
     }
+
+  const nameChangedHandler = (event) => {
+      setPersonsState({
+        persons: [
+        {name: "John", age: 28},
+        {name: event.target.value, age: 29},
+        ]
+   })
+  }
 
     return (
       <div>
         <h1>
           This is react.
         </h1>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+        <button onClick={switchNameHandler.bind(this, "John")}>Switch Name</button>
+        <Person 
+          name={personsState.persons[0].name} 
+          age={personsState.persons[0].age}
+          click={() => switchNameHandler("Johnny")}
+        />
+        <Person 
+          name={personsState.persons[1].name} 
+          age={personsState.persons[1].age}
+          changed={nameChangedHandler}
+        />
       </div>
     );
 }
--- BaseFeatures/base-features/src/Person/Person.js
+++ BaseFeatures/base-features/src/Person/Person.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const person = (props) => {
     return (
-        <div>
+        <div onClick={props.click}>
             <p>I'm a person. Name: {props.name} Age: {props.age}</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     );
 }
