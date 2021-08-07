98f880a2fc0dd92f91ee4174d5d582067731a2da
DavidLapadula <lapadula.david@gmail.com>
2021-08-07 15:44:45 -0400

    [effects-reducers-context] added state management with use context api

--- EffectsReducersContext/effects-reducers-context/src/components/MainHeader/Navigation.js
+++ EffectsReducersContext/effects-reducers-context/src/components/MainHeader/Navigation.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
+  
   return (
     <nav className={classes.nav}>
       <ul>
@@ -18,7 +21,7 @@ const Navigation = (props) => {
         )}
         {props.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
