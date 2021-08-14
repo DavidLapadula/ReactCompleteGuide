5ff32be2646e8319626507d8f769fa923cd62b39
DavidLapadula <lapadula.david@gmail.com>
2021-08-14 11:05:13 -0400

    [food-app] add cart item reducer for shopping app

--- FoodApp/food-app/src/components/Layout/HeaderCartButton.js
+++ FoodApp/food-app/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,23 @@
+import { useContext } from 'react';
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+    const cardCtx = useContext(CartContext);
+
+    const numberOfCartItems = cardCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
     return (
-        <button className={classes.button}>
+        <button className={classes.button} onClick={props.onClicks}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     );
