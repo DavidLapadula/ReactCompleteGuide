1e5585f4a1a6f385562af4836998d9c316b5a228
DavidLapadula <lapadula.david@gmail.com>
2021-08-24 22:29:34 -0400

    [behind-the-scenes] set up simple components to test virtual dom re-rendering

---
+++ BehindTheScenes/behind-the-scenes/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+import DemoOutput from './components/Demo/DemoOutput';
+import Button from './components/UI/Button/Button';
+import './App.css';
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraph = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph)
+  }
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraph}>Toggle</Button>
+    </div>
+  );
+}
+
+export default App;
