3aba47cec5b0905364c65a795bb18b434abbe1a2
DavidLapadula <lapadula.david@gmail.com>
2021-08-26 22:33:12 -0400

    [behind-the-scenes] tested prevent re-rendering with useCallback

--- BehindTheScenes/behind-the-scenes/src/App.js
+++ BehindTheScenes/behind-the-scenes/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
@@ -7,9 +7,9 @@ import './App.css';
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
-  const toggleParagraph = () => {
+  const toggleParagraph = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph)
-  }
+  }, []);
 
   return (
     <div className="app">
