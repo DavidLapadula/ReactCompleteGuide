09d3eac07be0734047939e56fb6bda2f076c2cc8
DavidLapadula <lapadula.david@gmail.com>
2021-10-18 21:48:37 -0400

    [deploying] practice lazy loading and building for deployment

--- ReactRouter/quote-application/src/App.js
+++ ReactRouter/quote-application/src/App.js
@@ -1,30 +1,35 @@
+import React, { Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+
 import AllQuotes from './pages/AllQuotes';
-import NewQuote from './pages/NewQuote';
 import QuoteDetail from './pages/QuoteDetail';
 import Layout from './components/layout/Layout';
 import NotFound from './components/quotes/NotFound';
 
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+
 function App() {
   return (
     <Layout>
-        <Switch>
-          <Route path='/' exact>
-            <Redirect to="/quotes"/>
-          </Route>
-          <Route path='/quotes' exact>
-            <AllQuotes />
-          </Route>
-          <Route path='/quotes/:quoteId'>
-            <QuoteDetail />
-          </Route>
-          <Route path='/new-quote'>
-            <NewQuote />
-          </Route>
-          <Route path='*'>
-            <NotFound />
-          </Route>
-        </Switch>
+      <Suspense fallback={<div>Loading</div>}>
+          <Switch>
+            <Route path='/' exact>
+              <Redirect to="/quotes"/>
+            </Route>
+            <Route path='/quotes' exact>
+              <AllQuotes />
+            </Route>
+            <Route path='/quotes/:quoteId'>
+              <QuoteDetail />
+            </Route>
+            <Route path='/new-quote'>
+              <NewQuote />
+            </Route>
+            <Route path='*'>
+              <NotFound />
+            </Route>
+          </Switch>
+        </Suspense>
     </Layout>
   );
 }
