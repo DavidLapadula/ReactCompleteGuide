77e1f03a2e45ad4804ecc2774e15b3adb6fb2e4c
DavidLapadula <lapadula.david@gmail.com>
2021-10-24 11:59:36 -0400

    [authentication] complete persistent login with local storage

--- Authentication/authentication/src/components/Auth/AuthForm.js
+++ Authentication/authentication/src/components/Auth/AuthForm.js
@@ -56,7 +56,8 @@ const AuthForm = () => {
               }
             })
             .then((data) => {
-              authCtx.login(data.idToken);
+              const expirationTime = new Date(new Date.getTime() + (+data.expiresIn * 1000)); // add current time in ms and add to expired time of token from server
+              authCtx.login(data.idToken, expirationTime);
               history.replace("/");
             })
             .catch((data) => console.log(data));
