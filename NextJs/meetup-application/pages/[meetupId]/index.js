9b3a6699e82e7c56a6a2ba9f987e76d5a4cd3c67
DavidLapadula <lapadula.david@gmail.com>
2021-10-30 12:54:52 -0400

    [nextjs] add specialized api to handle db requests

--- NextJs/meetup-application/pages/[meetupId]/index.js
+++ NextJs/meetup-application/pages/[meetupId]/index.js
@@ -1,6 +1,6 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
       image=""
@@ -9,6 +9,33 @@ function MeetupDetails() {
       description="First meet"
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: true,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupData: {
+        image: "",
+        title: "First",
+        address: "234 Street",
+        description: "First meet",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
--- NextJs/meetup-application/pages/index.js
+++ NextJs/meetup-application/pages/index.js
@@ -1,19 +1,38 @@
-import MeetupList from '../components/meetups/MeetupList';
+import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'First meetup',
-        image: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
-        address: '234 Street',
-        description: 'meeting 1'
-    }
+  {
+    id: "m1",
+    title: "First meetup",
+    image: "https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg",
+    address: "234 Street",
+    description: "meeting 1",
+  },
 ];
 
-function HomePage(){
-    return ( 
-        <MeetupList meetups={DUMMY_MEETUPS} />
-    );
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+// data fetching to server side during build process
+// export async function getStaticProps() {
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//     revalidate: 10,
+//   };
+// }
+
+export async function getServerSideProps(context) {
+  const req = context.req;
+  const res = context.res;
+
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
 }
 
 export default HomePage;
