c13cf9bcf0ebdcc934143e70d24642759d05ec73
DavidLapadula <lapadula.david@gmail.com>
2021-11-06 11:14:00 -0400

    [react-redux] complete custom store using only react hooks

--- ReplaceRedux/replace-redux/src/index.js
+++ ReplaceRedux/replace-redux/src/index.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "redux";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
+import "./index.css";
+import App from "./App";
+import configureProductStore from "./hooks-store/products-store";
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
+configureProductStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
